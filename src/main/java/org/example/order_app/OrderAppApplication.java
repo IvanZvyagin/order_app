8f01be06a7c61136145f6431a3bd61fde8c4d598
IvanZvyagin <ivan.zvyagin20@gmail.com>
2025-11-19 15:39:11 +0300

    2

--- src/main/java/org/example/order_app/OrderAppApplication.java
+++ src/main/java/org/example/order_app/OrderAppApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OrderAppApplication {
-
+//старт приложения
 	public static void main(String[] args) {
 		SpringApplication.run(OrderAppApplication.class, args);
 	}
